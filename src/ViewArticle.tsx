e77e89ac89129405e0222b517c462b022c1ae2ab
Benji <benji@gogetfetch.com>
2019-05-07 16:27:04 +0100

    .

--- src/ViewArticle.tsx
+++ src/ViewArticle.tsx
@@ -3,6 +3,12 @@ import React, { ReactElement } from 'react';
 /**
  * View Article
  *
+ * Here a user should be able to :
+ *
+ * View an article
+ * See related Articles
+ * Upvote an article (costs money)
+ * 
  */
 
 export default (): ReactElement => (
